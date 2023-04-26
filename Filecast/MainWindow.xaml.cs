d3cadbcedca3e6e8646742443dbb051daa76b8b7
philekiert <philekiert@live.co.uk>
2023-04-26 23:38:30 +0100

    First commit, barely working, just backing up.

--- Filecast/MainWindow.xaml.cs
+++ Filecast/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +15,34 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace Filecast
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            WaveOutEvent outputDevice = new WaveOutEvent();
+            AudioFileReader audioFile;
+
+
+            // Get files ending in .mp3, .wav or .wma
+            List<string> files = new List<string>();
+            string[] allFilesInDirectory = Directory.GetFiles(".", "*.*", SearchOption.TopDirectoryOnly);
+            foreach (string f in allFilesInDirectory)
+                if (f.EndsWith(".mp3") || f.EndsWith(".wav") || f.EndsWith("wma"))
+                    files.Add(f.Replace(".\\", ""));
+
+            if (files.Count > 0)
+            {
+                audioFile = new AudioFileReader(files[0]);
+                outputDevice.Init(audioFile);
+                outputDevice.Play(); 
+            }
         }
     }
 }
